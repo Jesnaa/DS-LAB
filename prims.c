8ce8b7df9c02a25df83ec9d8adb7e156eea1133b
Jesnamol Thomas <98266202+Jesnaa@users.noreply.github.com>
2022-03-11 18:55:13 +0530

    Add files via upload

---
+++ prims.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdbool.h>
+#define INF 9999999
+#define V 5
+int G[V][V] = {
+{0, 9, 75, 0, 0},
+{9, 0, 95, 19, 42},
+{75, 95, 0, 51, 66},
+{0, 19, 51, 0, 31},
+{0, 42, 66, 31, 0}};
+int main()
+{
+int no_edge; // number of edge
+int selected[V];
+memset(selected, false, sizeof(selected));
+no_edge = 0;
+selected[0] = true;
+int x;
+int y;
+printf("Edge : Weight\n");
+while (no_edge < V - 1)
+{
+int min = INF;
+x = 0;
+y = 0;
+int i,j;
+for ( i = 0; i < V; i++)
+{
+if (selected[i])
+{
+for ( j = 0; j < V; j++)
+{
+if (!selected[j] && G[i][j])
+{
+if (min > G[i][j]) {
+min = G[i][j];
+x = i;
+y = j;
+} } } } }
+printf("%d - %d : %d\n", x, y, G[x][y]);
+selected[y] = true;
+no_edge++;
+}
+return 0;
+}
